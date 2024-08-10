ca694a1fd797b924c04d4f930c1f325f96bd5d7a
Marcos Muñoz <marcosestudio13@gmail.com>
2024-08-10 15:11:15 -0300

    adding the product card along with its css

---
+++ app/views/components/card-product.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div class="product-grid">
+      <a href="/all/all.html" class="product-card">
+        <img
+          src="img/post de instagram descuentos electrodomésticos llamativo rojo.png"
+          alt="Product Image"
+        />
+        <div class="product-info">
+          <h2 class="product-title">Product Title</h2>
+          <p class="product-description">
+            This is a short description of the product.
+          </p>
+          <span class="product-price">$29.99</span>
+          <button class="add-to-cart">Add to Cart</button>
+        </div>
+      </a>
+    </div>
+  </body>
+</html>
