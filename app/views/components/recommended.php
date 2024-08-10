afce2eb86a94cef5e4d844d9b4c4298ab159e4b8
Marcos Muñoz <marcosestudio13@gmail.com>
2024-08-10 15:29:24 -0300

    adding the js folder, adding the recommended section along with its css and js

---
+++ app/views/components/recommended.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<body>
+    <div class="recommended-products-wrapper">
+        <button class="scroll-button left">&lt;</button>
+        <div class="recommended-products-container"> 
+            <div class="product">
+                <img src="#" alt="Producto 1" class="product-photo">
+                <h3 class="product-name">Nombre del Producto 1</h3>
+            </div>
+            <button class="scroll-button right">&gt;</button>
+        </div>
+    </div>
+
+</body>
+
+</html>
