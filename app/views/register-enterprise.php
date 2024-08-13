e5a3e4f08496c9e4e632e418deb5867add04ba6b
Marcos Muñoz <marcosestudio13@gmail.com>
2024-08-13 14:04:57 -0300

    aggregation of webpage login-user,login-enterprise,user-register,-enterprise-register.

---
+++ app/views/register-enterprise.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var array $products
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php loadCSS() ?>
+    <?php loadCSS("pages/homepage") ?>
+
+    <title>Homepage</title>
+</head>
+
+<body>
+    <?php view('components/top-header'); ?>
+    <main>
+         <?php view('components/register-enterprise-component'); ?>
+        <?php view('components/footer'); ?>
+    </main>
+    <?php
+    ?>
+</body>
+
+</html>
