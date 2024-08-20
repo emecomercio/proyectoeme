65c9434b1283f25352a1600a840f521443deb1ce
Luca Gomez <lucagomez195@gmail.com>
2024-08-20 12:02:28 -0300

    Update product-page.php
    link the favorite-button.js

--- app/views/products/product-page.php
+++ app/views/products/product-page.php
@@ -37,6 +37,7 @@
     <?php view("layout/footer") ?>
 </body>
 <?php
+loadjs("components/favorite-button");
 loadjs("components/user-button");
 loadJS("components/buy-button");
 loadJS("components/color-model-selector");
