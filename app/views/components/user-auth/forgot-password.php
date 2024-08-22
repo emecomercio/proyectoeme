9295d6dac91711fc62bdf0ce7e2f34408b60338f
Facundo Canclini <facundocanclini27@gmail.com>
2024-08-22 17:44:00 -0300

    Update font size, buttons and add T&C style
    -Updated font size on the password recovery page
    -Added button functionality on the password recovery page
    -Added styles and functional dropdown on the terms and conditions page

--- app/views/components/user-auth/forgot-password.php
+++ app/views/components/user-auth/forgot-password.php
@@ -1,11 +1,13 @@
 <div class="forgot-password">
     <h1>Recupera tu cuenta</h1>
-    <input type="email" placeholder="Ingrese su correo electronico">
-    <div class="button-container">
-        <button>Cancelar</button>
-        <button>Buscar</button>
-    </div>
+    <form>
+        <input type="email" id="email" placeholder="Ingrese su correo electrónico" required>
+        <div class="button-container">
+            <button type="button" onclick="window.location.href='/login-user'">Cancelar</button>
+            <button type="submit">Buscar</button>
+        </div>
+    </form>
     <a href="mailto:emecommerceoficial@gmail.com">
-        <p>No tengo acceso a mi correo electronico</p>
+        <p>No tengo acceso a mi correo electrónico</p>
     </a>
 </div>
