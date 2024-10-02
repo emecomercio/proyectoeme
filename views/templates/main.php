599d135ad516a9ad21759ba00b0fe5330c97c0f3
Luca Gomez <lucagomez195@gmail.com>
2024-10-02 20:50:10 -0300

    Update seller dashboard functionality
    - Modified seller_dashboard.js to enhance product creation and automatic
     refresh of the product list without reloading the page.
    - Updated login.php to streamline user login processes.
    - Revised dashboard.php for improved user experience and layout.
    - Adjusted main.php template to reflect changes in authentication and
     user role handling.

--- views/templates/main.php
+++ views/templates/main.php
@@ -46,7 +46,7 @@ $categories = getCategories();
     <header>
         <div class="main-h">
             <div class="logo">
-                <a href="/">
+            <a href="<?= getLogoHref() ?>">
                     <img class="LogoEme" src="<?= asset("/img/icons/logo.png") ?>" alt="logo de la empresa" />
                 </a>
             </div>
@@ -84,8 +84,8 @@ $categories = getCategories();
 
                     </div>
                 </div>
-
-                <div class=" TextoIcono" id="cart-menu">
+                
+                <div hidden class=" TextoIcono" id="cart-menu">
                     <img src="<?= asset("/img/icons/carrito_icono.png") ?>" class="icono" alt="Usuario" />
                     <br />Carrito
                     <div class="dropdown-content" style="display: none;">
@@ -96,20 +96,34 @@ $categories = getCategories();
         </div>
         <ul class="snd-h">
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" id="categoriesDropdown">Categorias</a>
-                <div class="dropdown-content" id="categoriesMenu">
-                    <?php
-                    foreach ($categories as $category):
-                    ?>
-                        <a href="/catalog/<?= $category['id'] ?>"><?= $category['name'] ?></a>
-                    <?php endforeach; ?>
-                </div>
-            </li>
-            <li class="dropdown"><a href="#">Ofertas</a></li>
-            <li class="dropdown"><a href="#">Cupones</a></li>
-            <li class="dropdown"><a href="#">Proximas ofertas</a></li>
-            <li class="dropdown"><a href="#">Ayuda</a></li>
+                    <a href="#" class="dropdown-toggle" id="categoriesDropdown">Categorias</a>
+                    <div class="dropdown-content" id="categoriesMenu">
+                        <?php foreach ($categories as $category): ?>
+                            <a href="/catalog/<?= $category['id'] ?>"><?= $category['name'] ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                </li>
+                <li class="dropdown"><a href="#">Ofertas</a></li>
+                <li class="dropdown"><a href="#">Cupones</a></li>
+                <li class="dropdown">
+                    <?php if (getUserRole() === 'seller'): ?>
+                        <a href="/seller/dashboard">Vender</a>
+                    <?php else: ?>
+                        <a href="#">Próximas ofertas</a>
+                    <?php endif; ?>
+                </li>
+                
+                <li class="dropdown"><a href="#">Ayuda</a></li>
         </ul>
+        <?php
+        function getLogoHref() {
+            if (getUserRole() == 'seller') {
+                return '/dashboard';
+            } else if (getUserRole() === 'buyer') {
+                return '/';
+            }
+        }
+        ?>
     </header>
 
     <main>
