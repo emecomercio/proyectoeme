a546f83e7e2308e0b6c1599a6b2965bc9a9f7a41
Anibal Boggio <anibalboggio12.6.2006@gmail.com>
2024-10-20 13:52:24 -0300

    Update 'modal' JS component
    - Changed the returned values to an array.
    - Added the possibility to select the buttons colors in the last param.

--- public/js/components/main/modal.js
+++ public/js/components/main/modal.js
@@ -1,4 +1,9 @@
-export function createModal(title, info, buttons = ["Confirmar", "Cancelar"]) {
+export function createModal(
+  title,
+  info,
+  buttons = ["Confirmar", "Cancelar"],
+  colors = ["#28a745", "#dc3545"]
+) {
   const modal = document.createElement("dialog");
   const id = title.toLowerCase().replace(/\s+/g, "-");
   modal.id = `alert-modal-${id}`;
@@ -21,16 +26,27 @@ export function createModal(title, info, buttons = ["Confirmar", "Cancelar"]) {
 
   const modalBody = document.createElement("div");
   modalBody.className = "modal-body";
-  modalBody.textContent = info;
+  modalBody.innerHTML = info;
 
   const modalFooter = document.createElement("div");
   modalFooter.className = "modal-footer";
   const confirmBtn = document.createElement("button");
   confirmBtn.className = "modal-btn confirm-btn";
   confirmBtn.textContent = buttons[0];
+  confirmBtn.onclick = () => {
+    closeModal(modal);
+  };
   const cancelBtn = document.createElement("button");
   cancelBtn.className = "modal-btn cancel-btn";
   cancelBtn.textContent = buttons[1];
+  cancelBtn.onclick = () => {
+    closeModal(modal);
+  };
+
+  if (colors.length > 0) {
+    confirmBtn.style.backgroundColor = colors[0];
+    cancelBtn.style.backgroundColor = colors[1];
+  }
 
   modalFooter.appendChild(cancelBtn);
   modalFooter.appendChild(confirmBtn);
@@ -40,7 +56,7 @@ export function createModal(title, info, buttons = ["Confirmar", "Cancelar"]) {
   modal.appendChild(modalFooter);
 
   document.body.appendChild(modal);
-  return { modal, confirmBtn, cancelBtn };
+  return [modal, confirmBtn, cancelBtn];
 }
 
 export function closeModal(modal, callback = () => {}) {
