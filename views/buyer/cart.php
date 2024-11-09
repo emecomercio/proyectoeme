0abb34d23fd640ddd7a72bc3dc01e3cc4d5b1b0a
Anibal Boggio <anibalboggio12.6.2006@gmail.com>
2024-11-08 23:55:32 -0300

    Fix bug on cart page

--- views/buyer/cart.php
+++ views/buyer/cart.php
@@ -1,22 +1,7 @@
-<script src="https://unpkg.com/i18next/i18next.min.js"></script>
-<script src="https://unpkg.com/i18next-http-backend/i18nextHttpBackend.min.js"></script>
-<h1>TU CARRITO</h1>
-<div class="cart-component-container">
-    <div class="cart-container">
-        <h3 class="h3">Tus productos agregados al carrito</h3>
+<h1 class="cart-title">TU CARRITO</h1>
+<section class="cart-container">
 
-        <div class="cart-container-content" id="cart-products">
-            <img class="icon-cart" src="<?= asset("/img/icons/carrito_icono.png") ?>" alt="">
-            <h3>¡Tu carrito está vacío!</h3>
-            <p>Agrega productos para empezar a comprar.</p>
-            <a href="/"> <button class="button-cart">Descubrir productos</button></a>
-        </div>
-    </div>
-    <div class="purchase-summary" id="purchase-summary">
-        <h3>Resumen de compra</h3>
-        <p>Aquí verás los importes de tu compra una vez que agregues productos.</p>
-    </div>
-</div>
+</section>
 <div class="payment-options">
     <div class="option">
         <h4 class="option-title">¡Cuotas sin interés!</h4>
@@ -45,28 +30,6 @@
         <div class="icons">
             <a href="#"><img src="https://via.placeholder.com/50x30?text=Prex" alt="Prex"></a>
             <a href="#"><img src="https://via.placeholder.com/50x30?text=Abitab" alt="Abitab"></a>
-        </div>
-    </div>
-    <div id="paymentModal" class="modal">
-        <div class="modal-content">
-            <span class="close">&times;</span>
-            <h2>Medios de pago</h2>
-            <p>Tarjetas prepagas, débito, y crédito hasta en 12 cuotas sin interés en pesos Uruguayos</p>
-            <p>Pagá hasta en 12 cuotas sin interés en pesos uruguayos durante este mes</p>
-            <div class="modal-icons">
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=VISA" alt="VISA"></a>
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=MasterCard" alt="MasterCard"></a>
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=OCA" alt="OCA"></a>
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=Amex" alt="American Express"></a>
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=Midinero" alt="Midinero"></a>
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=Lider" alt="Lider"></a>
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=Diners" alt="Diners Club"></a>
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=Prex" alt="Prex"></a>
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=Abitab" alt="Abitab"></a>
-                <a href="#"><img src="https://via.placeholder.com/50x30?text=OCA+Blue" alt="OCA Blue"></a>
-            </div>
-            <h3>Pagos a través de PayPal</h3>
-            <a href="#"><img src="https://via.placeholder.com/50x30?text=PayPal" alt="PayPal"></a>
         </div>
     </div>
 </div>
